5ca1ea84cc50efd10325eb8dc87ce4711de37f40
TU TRAN <vantutran2016@gmail.com>
2019-11-23 12:48:53 -0800

    rename db, restructure

--- src/main/java/com/assignment/ecom/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.assignment.ecom.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(Long id){
-        super("Could not find product "+id);
-    }
-}
